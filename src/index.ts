f81094d103464a66c88e592cf5721eb73a73600a
Yuxin <yuxin.huo@shopee.com>
2022-06-27 16:06:49 +0800

    stash
    stash

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import {getNthChild} from "./getNthChild";
 import {getTag} from "./getTag";
 import {isUnique} from "./isUnique";
 import {getParents} from "./getParents";
-import {optimizeSelector} from "./optimizeSelector";
+import {getSelector, optimizeSelector} from "./optimizeSelector";
 
 /**
  * Returns all the selectors of the elmenet
@@ -33,28 +33,23 @@ function getAllSelectors(el, selectors, attributesToIgnore) {
 }
 
 /**
- * Tests uniqueNess of the element inside its parent
- * @param  { Object } element
- * @param { String } Selectors
- * @return { Boolean }
- */
-function parentUnique(element, selector) {
-  const {parentNode} = element;
-  const elements = parentNode.querySelectorAll(selector);
-  return elements.length === 1 && elements[0] === element;
-}
-
-/**
  * Tests all selectors for uniqueness and returns the first unique selector.
  * @param  { Object } element
  * @param  { Array } selectors
  * @return { String }
  */
-function getFirstUnique(element, selectors) {
-  const globalUnique = selectors.find((selector) =>
-    isUnique(element, selector)
+function getFirstUnique(parentSelectors: string[], element, selectors) {
+  const notEmpty = selectors.filter((selector) => selector);
+  const soloUnique = notEmpty.find((selector) =>
+    isUnique(element, getSelector([selector]))
   );
-  return globalUnique || selectors.find(parentUnique.bind(null, element));
+  if (soloUnique) {
+    return soloUnique;
+  }
+  const combineUnique = notEmpty.find((selector) =>
+    isUnique(element, getSelector([...parentSelectors, selector]))
+  );
+  return combineUnique;
 }
 
 /**
@@ -64,24 +59,54 @@ function getFirstUnique(element, selectors) {
  * @param  { String } tag
  * @return { String }
  */
-function getUniqueCombination(element, items, tag) {
-  let combinations = getCombinations(items, 3),
-    firstUnique = getFirstUnique(element, combinations);
+function getUniqueCombination(
+  parentSelectors: string[],
+  element: Element,
+  id = "",
+  // class arr / attr arr
+  selectors: string[] = [],
+  tag = "",
+  nth = ""
+) {
+  console.log(`getUniqueCombination: ${id}, ${selectors}, ${tag}, ${nth}`);
+  // 0. id
+  // 1. class combine
+  const selectorItems = getCombinations(selectors, 3);
+  // 2. tag
+  const tags = tag ? [tag] : [];
+  // 3. tag + class
+  const tagSelectors = tag
+    ? selectorItems.map((selector) => tag + selector)
+    : [];
+  // 4. class + nth
+  const nthSelectors = nth
+    ? selectorItems.map((selector) => selector + nth)
+    : [];
+  // 5. tag + nth
+  const tagNths = tag && nth ? [tag + nth] : [];
+  // 6. tag + class + nth
+  const tagNthSelectors =
+    tag && nth ? selectorItems.map((selector) => tag + selector + nth) : [];
 
-  if (Boolean(firstUnique)) {
-    return firstUnique;
-  }
+  const allSelectorItems = [
+    id,
+    ...selectorItems,
+    ...tags,
+    ...tagSelectors,
+    ...nthSelectors,
+    ...tagNths,
+    ...tagNthSelectors,
+  ];
 
-  if (Boolean(tag)) {
-    combinations = combinations.map((combination) => tag + combination);
-    firstUnique = getFirstUnique(element, combinations);
+  console.log("allSelectorItems", allSelectorItems);
 
-    if (Boolean(firstUnique)) {
-      return firstUnique;
-    }
-  }
+  const firstUnique = getFirstUnique(
+    parentSelectors,
+    element,
+    allSelectorItems
+  );
 
-  return null;
+  return firstUnique || null;
 }
 
 /**
@@ -91,6 +116,7 @@ function getUniqueCombination(element, items, tag) {
  * @return { String }
  */
 function getUniqueSelector(
+  parentSelectors: string[],
   element: Element,
   selectorTypes,
   attributesToIgnore,
@@ -113,46 +139,23 @@ function getUniqueSelector(
     );
   }
 
-  for (let selectorType of selectorTypes) {
-    const {ID, Tag, Class: Classes, Attributes, NthChild} = elementSelectors;
-    switch (selectorType) {
-      case "ID":
-        if (Boolean(ID) && parentUnique(element, ID)) {
-          return ID;
-        }
-        break;
+  const {
+    ID = "",
+    Class: Classes = [],
+    Tag = "",
+    Attributes = [],
+    NthChild = "",
+  } = elementSelectors;
 
-      case "Tag":
-        if (Boolean(Tag) && parentUnique(element, Tag)) {
-          return Tag;
-        }
-        break;
-
-      case "Class":
-        if (Boolean(Classes) && Classes.length) {
-          foundSelector = getUniqueCombination(element, Classes, Tag);
-          if (foundSelector) {
-            return foundSelector;
-          }
-        }
-        break;
-
-      case "Attributes":
-        if (Boolean(Attributes) && Attributes.length) {
-          foundSelector = getUniqueCombination(element, Attributes, Tag);
-          if (foundSelector) {
-            return foundSelector;
-          }
-        }
-        break;
-
-      case "NthChild":
-        if (Boolean(NthChild)) {
-          return NthChild;
-        }
-    }
-  }
-  return null;
+  foundSelector = getUniqueCombination(
+    parentSelectors,
+    element,
+    ID,
+    [...Classes, ...Attributes],
+    Tag,
+    NthChild
+  );
+  return foundSelector || null;
 }
 
 /**
@@ -166,6 +169,7 @@ function getUniqueSelector(
 enum SelectorTypeEnum {
   ID = "ID",
   Class = "Class",
+  Attributes = "Attributes",
   Tag = "Tag",
   NthChild = "NthChild",
 }
@@ -188,25 +192,27 @@ export default function unique(
   const parents = getParents(el);
 
   for (let typeIndex = 0; typeIndex < selectorTypes.length; typeIndex++) {
+    // 对每一个优先级，计算一次
     // 优先级 >= 当前的 type
     const types = selectorTypes.slice(0, typeIndex + 1);
     const allSelectors: string[] = [];
     console.log("types", types);
 
-    // 对每一个优先级，计算一次
-    for (let i = 0; i < parents.length; i++) {
+    // 循环顺序：父 -> 子
+    for (let i = parents.length - 1; i >= 0; i--) {
       const el = parents[i];
       const selector = getUniqueSelector(
+        allSelectors,
         el,
         types,
         attributesToIgnore,
         excludeRegex
       );
       // 取不到目标元素上 parent 下唯一的选择器，直接返回
-      if (i === 0 && !selector) {
+      if (i === parents.length - 1 && !selector) {
         break;
       }
-      allSelectors.unshift(selector);
+      allSelectors.push(selector);
     }
 
     console.log("allSelectors", allSelectors);
--- src/optimizeSelector.ts
+++ src/optimizeSelector.ts
@@ -4,10 +4,30 @@ export const getSelector = (selectors: (string | null)[]) =>
   selectors
     // 拼接
     .join(" > ")
-    // 去掉头部 >
+    // 去掉头部尾部 >
     .replace(/^( > )+/, "")
+    .replace(/( > )+$/, "")
     // 去掉相连达到两个以上的 >
     .replace(/( > ){2,}/g, " ");
+
+const checkSelector = (
+  el: Element,
+  necessary: (string | null)[],
+  index: number
+) => {
+  const selectors = [...necessary];
+  // 去掉当前 selector
+  selectors[index] = null;
+  const selectorStr = getSelector(selectors);
+  console.log(
+    `test remove ${index} --- ${necessary[index]} --- ${selectorStr}`
+  );
+  console.log("still unique", isUnique(el, selectorStr));
+  // 依然唯一定位该元素
+  if (isUnique(el, selectorStr)) {
+    necessary[index] = null;
+  }
+};
 
 export const optimizeSelector = (
   el: Element,
@@ -19,23 +39,24 @@ export const optimizeSelector = (
 
   console.log("[before] allSelectors", allSelectors);
   const necessary = [...allSelectors];
-  allSelectors.forEach((selector, index) => {
+
+  // 1. 去掉 优先级低的 tag\nth
+  necessary.forEach((selector, index) => {
+    if (!selector || selector.match(/[#.=]/)) {
+      return;
+    }
+    checkSelector(el, necessary, index);
+  });
+
+  console.log("[after1] necessary", necessary);
+  // 2. 去掉 优先级高的 class\id
+  necessary.forEach((selector, index) => {
     if (!selector) {
       return;
     }
-    const selectors = [...necessary];
-    // 去掉当前 selector
-    selectors.splice(index, 1);
-    const selectorStr = getSelector(selectors);
-    console.log(
-      `test remove ${index} --- ${necessary[index]} --- ${selectorStr}`
-    );
-    console.log("still unique", isUnique(el, selectorStr));
-    // 依然唯一定位该元素
-    if (isUnique(el, selectorStr)) {
-      necessary[index] = null;
-    }
+    checkSelector(el, necessary, index);
   });
-  console.log("[after] necessary", necessary);
+
+  console.log("[after2] necessary", necessary);
   return getSelector(necessary);
 };
