c81f6f0300a91540191c00a6ed4abd1abefad4cd
Yuxin <yuxin.huo@shopee.com>
2023-09-22 18:17:41 +0800

    1.0.3

--- src/getAttributes.ts
+++ src/getAttributes.ts
@@ -6,13 +6,17 @@
  */
 export function getAttributes(el: Element, attributesRegExp: string[] = []) {
   const { attributes } = el;
+  console.log("[short-selector] attributes", attributes);
 
   // 按先后顺序
   return attributesRegExp.reduce((sum: string[], regexp: string) => {
-    const attr = Array.from(attributes).find((attr) => attr.nodeName.match(new RegExp(regexp)));
-    if (attr) {
-      sum.push(`[${attr.nodeName}="${attr.value}"]`);
+    var attrList = Array.from(attributes).filter(function (attr) {
+      return attr.nodeName.match(new RegExp(regexp));
+    });
+    if (attrList?.length) {
+      attrList.forEach((attr) => sum.push(`[${attr.nodeName}="${attr.value}"]`));
     }
+    console.log("[short-selector] sum", sum);
     return sum;
   }, []);
 }
